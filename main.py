d3069cb35e31eca61e2fb36b63e99ce4e5f87df5
Bidasep <tornado16@hotmail.com>
2026-06-04 17:52:54 -0300

    Feature/Menu

---
+++ main.py
@@ -0,0 +1,99 @@
+#O objetivo deste projeto é desenvolver um programa para gerenciar uma Pokédex (um banco de dados de Pokémon). O 
+# programa deve permitir ao usuário realizar operações básicas de gerenciamento, como adicionar,
+# listar, remover e atualizar dados de Pokémon, além de registrar capturas e exibir um histórico de 
+# capturas. Este projeto ajuda a consolidar o uso de estruturas de dados em Python, como dicionários e 
+# listas, bem como a aplicar conceitos de programação orientada a objetos, se desejado, e manipulação de entrada e saída.
+
+#1 - Adicionar Pokémon:
+# O programa deve pedir o nome do Pokémon, o tipo (como "Fogo", "Água", "Elétrico", etc.),
+# e o nível (um número inteiro entre 1 e 100). Essas informações devem ser armazenadas no dicionário.
+
+#Listar Pokémon:
+# O programa deve exibir todos os Pokémon cadastrados no formato: 
+# nome do Pokémon - tipo - nível. Os Pokémon devem ser listados em ordem alfabética.
+
+#Remover Pokémon:
+# O programa deve pedir o nome do Pokémon a ser removido. Caso o Pokémon 
+# exista, ele será removido do dicionário. Se o Pokémon não existir, o programa exibirá uma mensagem de erro.
+
+#Atualizar nível do Pokémon:
+# O programa deve pedir o nome do Pokémon e o novo nível (um número inteiro entre 1 e 100).
+# Se o Pokémon existir, o nível será atualizado. Caso contrário, será exibida uma mensagem de erro.
+
+#Registrar captura de Pokémon:
+# O programa deve pedir o nome do Pokémon e a quantidade de 
+# vezes capturadas. O valor de capturas será atualizado, aumentando a quantidade de vezes que o 
+# Pokémon foi capturado. Caso o Pokémon não exista, será exibida uma mensagem de erro.
+
+#Exibir histórico de capturas:
+# O histórico de capturas deve ser armazenado em uma lista, onde 
+# cada entrada consiste no nome do Pokémon e a quantidade de vezes que foi capturado. Quando o 
+# usuário escolher exibir o histórico de capturas, o programa deve mostrar todas as entradas feitas.
+
+#Sair: 
+# O programa deve encerrar sua execução quando o usuário escolher a opção de sair.
+
+def adicionar_pokemon():
+    return print("adicionar_pokemon")
+
+def listar_pokemon():
+    return print("listar_pokemon")
+
+def remover_pokemon():
+    return print("remover_pokemon")
+   
+def atualizar_nivel():
+    return print("atualizar_nivel")
+
+def registrar_captura():
+    return print("registrar_captura")
+
+def exibir_historico():
+    return print("exibir_historico")
+    
+
+
+
+def menu():
+    print("\n======================================================")
+    print("   Seja Bem vindo ao sistema de gerenciamento Pokemon.")
+    print("======================================================")
+    print("Escolha uma opção: \n")
+    print(" 1 - Adicionar pokemon")
+    print(" 2 - Listar Pokemons")
+    print(" 3 - Remover Pokemons")
+    print(" 4 - Atualizar nivel do pokemon")
+    print(" 5 - Registar Captura pokemon")
+    print(" 6 - Exibir histórico de capturas")
+    print(" 7 - Sair\n")
+    
+    
+def main():
+    
+    while True:
+        menu()
+        
+        opcao = input ("Digite a opção Escolhida : \n")
+        menu()
+        if opcao == "1":
+            adicionar_pokemon()
+        elif opcao == "2":
+            listar_pokemon()
+        elif opcao == "3":
+            remover_pokemon()
+        elif opcao == "4":
+            atualizar_nivel()
+        elif opcao == "5":
+            registrar_captura()
+        elif opcao == "6":
+            exibir_historico()
+        elif opcao == "7":
+            print("Sainda. Obrigado...")
+            break
+            
+        else:
+            print ("Opção inválida, Digite uma opção válida.")   
+            
+#função principal iniciar
+
+main()     
